302cd74d0fcfbf39ff45839bf7f585b7c307f1af
progfay <shun1856shun@gmail.com>
2019-06-22 05:33:06 +0900

    test: add test of plain text

---
+++ __tests__/plain.test.ts
@@ -0,0 +1,39 @@
+/* global describe it expect */
+
+import { LineComponentType } from '../src/types'
+import convertToLineComponent from '../src/convertToLineComponents'
+import parseToLines from '../src/parseToLines'
+
+describe('plain', () => {
+  it('Simple plain text', () => {
+    const input = 'Plain text'
+    const lineComponents: Array<LineComponentType> = convertToLineComponent(input)
+    expect(parseToLines(lineComponents)).toEqual([
+      {
+        indent: 0,
+        nodes: [
+          {
+            type: 'plain',
+            text: 'Plain text'
+          }
+        ]
+      }
+    ])
+  })
+
+  it('Keep tail space', () => {
+    const input = 'Tail space ->  '
+    const lineComponents: Array<LineComponentType> = convertToLineComponent(input)
+    expect(parseToLines(lineComponents)).toEqual([
+      {
+        indent: 0,
+        nodes: [
+          {
+            type: 'plain',
+            text: 'Tail space ->  '
+          }
+        ]
+      }
+    ])
+  })
+})
