3068547cbefa1bb6632cc9a95c93bbd098eb84a4
progfay <shun1856shun@gmail.com>
2019-06-21 21:44:28 +0900

    style: use eslint (Standard Style)

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'standard',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+    'project': './tsconfig.json'
+  },
+  'plugins': ['@typescript-eslint'],
+  'rules': {
+  }
+}
