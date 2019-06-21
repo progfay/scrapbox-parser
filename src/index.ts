ba94347402504d9fadabf1178f8cc954d0b92573
progfay <shun1856shun@gmail.com>
2019-06-22 00:23:47 +0900

    feat: parse lines for PlainNode (WIP)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-const parse = (input: string): any => {
-  return {}
+const parse = (input: string): PageType => {
+  const _lines: Array<string> = input.trim().split('\n')
+  const title: string = _lines.shift() || 'Untitled'
+  const lines: LineType[] = _lines.map((line: string): LineType => {
+    const indentMatcher = line.match(/^\s*/)
+    const indent: number = indentMatcher ? indentMatcher[0].length : 0
+    return {
+      indent,
+      nodes: [
+        {
+          type: 'plain',
+          text: line.trim()
+        }
+      ]
+    }
+  })
+  return { title, lines }
 }
 
 export default parse
