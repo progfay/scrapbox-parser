e470526dfabb0b0ce233bf203b8c4fa9d2baeef8
progfay <shun1856shun@gmail.com>
2019-06-24 06:43:39 +0900

    test(page): update and correct answer

--- __tests__/page/index.test.ts
+++ __tests__/page/index.test.ts
@@ -1,16 +1,20 @@
 /* global describe it expect */
 
 import * as fs from 'fs'
-import { BlockComponentType, convertToBlockComponents } from '../../src/block/BlockComponent'
-import { BlockType } from '../../src/block'
-import { convertToBlocks } from '../../src/parse'
+import parse from '../../src'
 
 describe('page', () => {
+  it('Empty page', () => {
+    const input = ''
+    expect(parse(input)).toEqual({
+      title: 'Untitled',
+      blocks: []
+    })
+  })
+
   it('https://scrapbox.io/help/Syntax', () => {
     const input = fs.readFileSync('./__tests__/page/input.txt').toString()
-    const blockComponents: Array<BlockComponentType> = convertToBlockComponents(input)
-    const blocks: Array<BlockType> = convertToBlocks(blockComponents)
     const answer = JSON.parse(fs.readFileSync('./__tests__/page/answer.json').toString())
-    expect(blocks).toEqual(answer)
+    expect(parse(input)).toEqual(answer)
   })
 })
