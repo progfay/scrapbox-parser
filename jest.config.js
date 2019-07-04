36499c089b1a54a9c1bff7d5af535e55281bcd5a
progfay <shun1856shun@gmail.com>
2019-07-04 18:59:44 +0900

    chore: split configuration of jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleFileExtensions: [
+    'ts',
+    'js'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: [
+    '**/tests/**/*.test.ts'
+  ]
+}
