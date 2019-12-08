80c6b454fd28ee47efda24eaf41b9ed7b10053a7
progfay <shun1856shun@gmail.com>
2019-12-09 00:43:24 +0900

    refactor(CombinedStream): write more simple

--- src/stream/CombinedStream.ts
+++ src/stream/CombinedStream.ts
@@ -1,19 +1,18 @@
 import { PassThrough, Writable } from 'stream'
 
 export default class CombinedStream extends PassThrough {
-  stream: Writable = this
+  stream: Writable
 
   constructor (...streams: Writable[]) {
     super({ objectMode: true })
 
-    for (const destination of streams) {
-      this.stream = this.stream.pipe(destination)
-    }
+    this.stream = streams.reduce(
+      (source, destination) => source.pipe(destination),
+      this as Writable
+    )
 
-    this.pipe = this._pipe.bind(this)
-  }
-
-  _pipe<T extends NodeJS.WritableStream> (destination: T, options?: { end?: boolean; }): T {
-    return this.stream.pipe(destination, options)
+    this.pipe = (destination, options) => (
+      this.stream.pipe(destination, options)
+    )
   }
 }
