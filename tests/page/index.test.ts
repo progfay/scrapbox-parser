80d9ce426af89efd6b71e59a56a8bd923179d74e
progfay <shun1856shun@gmail.com>
2019-12-20 10:18:00 +0900

    test(page): add test for `parse` without option

--- tests/page/index.test.ts
+++ tests/page/index.test.ts
@@ -8,6 +8,11 @@ describe('page', () => {
     expect(input).toMatchSnapshotWhenParsing({ hasTitle: true })
   })
 
+  it('Title Block without `hasTitle` option', () => {
+    const input = 'Title'
+    expect(input).toMatchSnapshotWhenParsing()
+  })
+
   it('https://scrapbox.io/help/Syntax', () => {
     const input = fs.readFileSync('./tests/page/input.txt').toString()
     expect(input).toMatchSnapshotWhenParsing({ hasTitle: true })
