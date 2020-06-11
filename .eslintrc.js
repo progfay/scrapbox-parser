9b4cf89e448a61c3b9547654e826d114ab274c78
progfay <shun1856shun@gmail.com>
2020-06-12 00:00:28 +0900

    style: enable eslint rules partially

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,11 +25,7 @@ module.exports = {
   'rules': {
     '@typescript-eslint/consistent-type-assertions': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/method-signature-style': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
     '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
     '@typescript-eslint/prefer-nullish-coalescing': 'off',
     '@typescript-eslint/prefer-optional-chain': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off'
