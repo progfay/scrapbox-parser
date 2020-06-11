0519d507489945a2ce6eb8bcf02ecafb8a7d906c
progfay <shun1856shun@gmail.com>
2020-06-11 23:31:35 +0900

    style: disabled some eslint-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     'browser': true,
     'es6': true
   },
-  'extends': 'standard',
+  'extends': [
+    'standard',
+    'standard-with-typescript'
+  ],
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly'
@@ -20,6 +23,15 @@ module.exports = {
     'jest'
   ],
   'rules': {
-    '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/prefer-optional-chain': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off'
   }
 }
