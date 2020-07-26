feaf5980d938e14207e3da2235e749fcc943b19a
progfay <shun1856shun@gmail.com>
2020-07-26 20:18:10 +0900

    test: add case icon and internal link on same line

--- tests/line/icon.test.ts
+++ tests/line/icon.test.ts
@@ -10,4 +10,8 @@ describe('icon', () => {
   it('Multiple icons', () => {
     expect('[me.icon*3]').toMatchSnapshotWhenParsing({ hasTitle: false })
   })
+
+  it('Icon and internal link on same line', () => {
+    expect('[Internal link][me.icon]').toMatchSnapshotWhenParsing({ hasTitle: false })
+  })
 })
