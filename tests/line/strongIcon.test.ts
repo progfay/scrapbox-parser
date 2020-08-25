c2994285157ae61dcb19486c34bdb100c5eaad34
progfay <shun1856shun@gmail.com>
2020-08-25 23:53:41 +0900

    test: add test case for different icons deeply

--- tests/line/strongIcon.test.ts
+++ tests/line/strongIcon.test.ts
@@ -1,3 +1,5 @@
+import { parse } from '../../src'
+
 describe('strongIcon', () => {
   it('Simple root strong icon', () => {
     expect('[[/icons/+1.icon]]').toMatchSnapshotWhenParsing({ hasTitle: false })
@@ -13,5 +15,15 @@ describe('strongIcon', () => {
 
   it('Strong icon and internal link on same line', () => {
     expect('[Internal link][[me.icon]]').toMatchSnapshotWhenParsing({ hasTitle: false })
+  })
+
+  it('Each multiple strong icon must be different Object', () => {
+    const [block] = parse('[[me.icon*2]]', { hasTitle: false })
+    if (block.type !== 'line') {
+      throw new Error('fail')
+    }
+
+    const [strongIcon1, strongIcon2] = block.nodes
+    expect(strongIcon1 === strongIcon2).toBe(false)
   })
 })
