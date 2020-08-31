8f6f4375eb09cedd86f042780055d18c6625f9df
progfay <shun1856shun@gmail.com>
2020-08-31 18:37:35 +0900

    refactor(parser): use createNodeParser in StrongImageNode

--- src/block/node/StrongImageNode.ts
+++ src/block/node/StrongImageNode.ts
@@ -1,42 +1,26 @@
-import { convertToLineNodes } from '.'
+import { createNodeParser } from './creator'
 
-import type { NodeParser } from '.'
+import type { NodeCreator } from './creator'
 
-const strongImageRegExp = /^(?<left>.*?)\[\[(?<src>https?:\/\/[^\s\]]+\.(png|jpe?g|gif|svg))\]\](?<right>.*)$/i
-const gyazoStrongImageRegExp = /^(?<left>.*?)\[\[(?<src>https?:\/\/([0-9a-z-]+\.)?gyazo\.com\/[0-9a-f]{32})\]\](?<right>.*)$/
+const strongImageRegExp = /^(.*?)(\[\[https?:\/\/[^\s\]]+\.(?:png|jpe?g|gif|svg)\]\])(.*)$/i
+const strongGyazoImageRegExp = /^(.*?)(\[\[https?:\/\/(?:[0-9a-z-]+\.)?gyazo\.com\/[0-9a-f]{32}\]\])(.*)$/
 
 export interface StrongImageNode {
   type: 'strongImage'
   src: string
 }
 
-interface StrongImageMatch {
-  groups: {
-    left: string
-    right: string
-    src: string
+const createStrongImageNode: NodeCreator<StrongImageNode> = target => {
+  const src = target.substring(2, target.length - 2)
+  const isGyazoImage = /^https?:\/\/([0-9a-z-]\.)?gyazo\.com\/[0-9a-f]{32}$/.test(src)
+  return {
+    type: 'strongImage',
+    src: isGyazoImage ? `${src}/thumb/1000` : src
   }
 }
 
-const isStrongImageMatch = (obj: any): obj is StrongImageMatch => obj?.groups?.src !== undefined
-
-const createStrongImageNode = (src: string): StrongImageNode => ({
-  type: 'strongImage',
-  src
+export const StrongImageNodeParser = createNodeParser(createStrongImageNode, {
+  parseOnNested: false,
+  parseOnQuoted: true,
+  patterns: [strongImageRegExp, strongGyazoImageRegExp]
 })
-
-export const StrongImageNodeParser: NodeParser = (text, { nested, quoted }, next) => {
-  if (nested) return next()
-
-  const StrongImageMatch = text.match(strongImageRegExp) ?? text.match(gyazoStrongImageRegExp)
-  if (!isStrongImageMatch(StrongImageMatch)) return next()
-
-  const { left, src, right } = StrongImageMatch.groups
-  const isGyazoImage = /^https?:\/\/([0-9a-z-]\.)?gyazo\.com\/[0-9a-f]{32}$/.test(src)
-
-  return [
-    ...convertToLineNodes(left, { nested, quoted }),
-    createStrongImageNode(src + (isGyazoImage ? '/thumb/1000' : '')),
-    ...convertToLineNodes(right, { nested, quoted })
-  ]
-}
