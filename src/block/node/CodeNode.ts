ab193fedf53fe2a095de6f2e23801d85a5f962f4
yuta0801 <yuta030801@gmail.com>
2020-09-03 10:30:44 +0900

    remove: command line notation from CodeNode

--- src/block/node/CodeNode.ts
+++ src/block/node/CodeNode.ts
@@ -3,7 +3,6 @@ import { createNodeParser } from './creator'
 import type { NodeCreator } from './creator'
 
 const codeRegExp = /^(.*?)(`.*?`)(.*)$/
-const codeCommandRegExp = /^()(\$ .+)()$/
 
 export interface CodeNode {
   type: 'code'
@@ -12,11 +11,11 @@ export interface CodeNode {
 
 const createCodeNode: NodeCreator<CodeNode> = target => ({
   type: 'code',
-  text: target.startsWith('$') ? target : target.substring(1, target.length - 1)
+  text: target.substring(1, target.length - 1)
 })
 
 export const CodeNodeParser = createNodeParser(createCodeNode, {
   parseOnNested: false,
   parseOnQuoted: true,
-  patterns: [codeRegExp, codeCommandRegExp]
+  patterns: [codeRegExp]
 })
