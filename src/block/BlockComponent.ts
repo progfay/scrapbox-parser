2e7b7af95dbe0a6c1326d3e15fbd75b9c232a039
progfay <shun1856shun@gmail.com>
2020-11-01 23:59:44 +0900

    refactor: rename convertToBlockComponents to parseToBlockComponents

--- src/block/BlockComponent.ts
+++ src/block/BlockComponent.ts
@@ -3,7 +3,7 @@ export interface BlockComponent {
   text: string
 }
 
-export const convertToBlockComponents = (input: string): BlockComponent[] =>
+export const parseToBlockComponents = (input: string): BlockComponent[] =>
   input.split('\n').map(text => ({
     indent: /^\s+/.exec(text)?.[0].length ?? 0,
     text
