c3173420a56ef0c92ba92890e00e10590c4dc070
progfay <shun1856shun@gmail.com>
2021-05-06 15:35:49 +0900

    feat: no-use lookbehind RegExp

--- src/block/node/HashTagNode.ts
+++ src/block/node/HashTagNode.ts
@@ -1,15 +1,35 @@
 import { createNodeParser } from './creator'
 
-import type { HashTagNode } from './type'
+import type { HashTagNode, PlainNode } from './type'
 import type { NodeCreator } from './creator'
 
-const hashTagRegExp = /(?<=^| )#\S+/
+const hashTagRegExp = /(?:^|\s)#\S+/
 
-const createHashTagNode: NodeCreator<HashTagNode> = raw => ({
-  type: 'hashTag',
-  raw,
-  href: raw.substring(1)
-})
+const createHashTagNode: NodeCreator<HashTagNode | PlainNode> = raw => {
+  if (raw.startsWith('#')) {
+    return {
+      type: 'hashTag',
+      raw,
+      href: raw.substring(1)
+    }
+  }
+
+  const space = raw.substring(0, 1)
+  const tag = raw.substring(1)
+
+  return [
+    {
+      type: 'plain',
+      raw: space,
+      text: space
+    },
+    {
+      type: 'hashTag',
+      raw: tag,
+      href: tag.substring(1)
+    }
+  ]
+}
 
 export const HashTagNodeParser = createNodeParser(createHashTagNode, {
   parseOnNested: false,
