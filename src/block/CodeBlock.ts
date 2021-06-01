e3ddd7ac4b6e7c77d1b7e9d453eabfbe6304188d
takker99 <37929109+takker99@users.noreply.github.com>
2021-06-01 23:04:01 +0900

    feat(parser): parse filename extensions
    - add the property `lang` in `CodeBlock`
    - enable to parse filenames like `hello.js(ts)`

--- src/block/CodeBlock.ts
+++ src/block/CodeBlock.ts
@@ -9,6 +9,7 @@ export interface CodeBlock {
   indent: number
   type: 'codeBlock'
   fileName: string
+  lang: string
   content: string
 }
 
@@ -17,12 +18,14 @@ export const convertToCodeBlock = (pack: CodeBlockPack): CodeBlock => {
     rows: [head, ...body]
   } = pack
   const { indent = 0, text = '' } = head ?? {}
-  const fileName: string = text.replace(/^\s*code:/, '')
+  const fileName: string = text.replace(/^\s*code:/, '').replace(/\([^()]+\)$/, '')
+  const lang: string = text.match(/\(([^()]+)\)$/)?.[1] ?? fileName.split('.').pop() ?? fileName
 
   return {
     indent,
     type: 'codeBlock',
     fileName,
+    lang,
     content: body.map((row: Row): string => row.text.substring(indent + 1)).join('\n')
   }
 }
