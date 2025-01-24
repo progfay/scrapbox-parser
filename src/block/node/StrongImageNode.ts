2fefb8d390329b61f6142ddfe3b1d784c5c2a32c
progfay <progfay@gmail.com>
2025-01-24 12:47:49 +0900

    chore: `npm run format`

--- src/block/node/StrongImageNode.ts
+++ src/block/node/StrongImageNode.ts
@@ -4,7 +4,8 @@ import { createNodeParser } from "./creator.ts";
 import type { NodeCreator } from "./creator.ts";
 import type { PlainNode, StrongImageNode } from "./type.ts";
 
-const strongImageRegExp = /\[\[https?:\/\/[^\s\]]+\.(?:png|jpe?g|gif|svg|webp)\]\]/i;
+const strongImageRegExp =
+	/\[\[https?:\/\/[^\s\]]+\.(?:png|jpe?g|gif|svg|webp)\]\]/i;
 const strongGyazoImageRegExp =
 	/\[\[https?:\/\/(?:[0-9a-z-]+\.)?gyazo\.com\/[0-9a-f]{32}\]\]/;
 
