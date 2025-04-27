e3279de5188958ff734e21d1027e831c18c792e5
progfay <19568747+progfay@users.noreply.github.com>
2025-04-27 11:21:18 +0900

    `npm run format`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,34 +1,34 @@
 import { resolve } from "node:path";
-import { defineConfig, type Options } from "tsdown";
+import { type Options, defineConfig } from "tsdown";
 
 type ModuleFormat = Extract<Options["format"], string>;
 
 const FORMAT_ENTRY_FILE_NAMES_MAP = {
-  es: "[name].mjs",
-  esm: "[name].mjs",
-  module: "[name].mjs",
-  cjs: "[name].cjs",
-  commonjs: "[name].cjs",
-  umd: "scrapbox-parser.umd.js",
-  iife: undefined,
+	es: "[name].mjs",
+	esm: "[name].mjs",
+	module: "[name].mjs",
+	cjs: "[name].cjs",
+	commonjs: "[name].cjs",
+	umd: "scrapbox-parser.umd.js",
+	iife: undefined,
 } as const satisfies Record<ModuleFormat, string | undefined>;
 
 export default defineConfig([
-  {
-    format: ["es", "cjs", "umd"],
-    entry: resolve(import.meta.dirname, "src/index.ts"),
-    dts: true,
-    minify: true,
-    sourcemap: true,
-    platform: "neutral",
-    outDir: "out",
-    clean: true,
-    outputOptions: ({ format, entryFileNames, ...options }) => ({
-      ...options,
-      format,
-      name: "ScrapboxParser",
-      entryFileNames:
-        (format && FORMAT_ENTRY_FILE_NAMES_MAP[format]) ?? entryFileNames,
-    }),
-  },
+	{
+		format: ["es", "cjs", "umd"],
+		entry: resolve(import.meta.dirname, "src/index.ts"),
+		dts: true,
+		minify: true,
+		sourcemap: true,
+		platform: "neutral",
+		outDir: "out",
+		clean: true,
+		outputOptions: ({ format, entryFileNames, ...options }) => ({
+			...options,
+			format,
+			name: "ScrapboxParser",
+			entryFileNames:
+				(format && FORMAT_ENTRY_FILE_NAMES_MAP[format]) ?? entryFileNames,
+		}),
+	},
 ]);
