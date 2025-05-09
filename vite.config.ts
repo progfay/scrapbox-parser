2edbb0fa0dc7b18559dbe5e33a265efc072cb985
progfay <19568747+progfay@users.noreply.github.com>
2025-05-10 01:47:49 +0900

    publish only-ESM code
    - ref. https://nodejs.org/en/learn/modules/publishing-a-package

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
 import { resolve } from "node:path";
-import { type LibraryFormats, defineConfig } from "vite";
-
-const formats = ["es", "cjs", "umd"] satisfies LibraryFormats[];
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	build: {
 		emptyOutDir: false,
 		outDir: "out",
 		lib: {
-			formats,
+			formats: ["es"],
 			entry: resolve(__dirname, "src/index.ts"),
-			name: "ScrapboxParser",
-			fileName: (format) => {
-				switch (format) {
-					case "cjs":
-						return "index.cjs";
-					case "es":
-						return "index.mjs";
-					case "umd":
-						return "scrapbox-parser.umd.js";
-					default:
-						throw new Error(`unknown format: ${format}`);
-				}
-			},
+			fileName: "index",
 		},
 	},
 });
