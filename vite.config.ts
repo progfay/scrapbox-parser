ae73f0e1b4f30a75b307d3ce035e6ed2991b1c3d
Shunsuke Mano <19568747+progfay@users.noreply.github.com>
2026-03-15 21:42:19 +0900

    chore(oxlint): remove unused lint rules (#2057)

--- vite.config.ts
+++ vite.config.ts
@@ -15,13 +15,6 @@ export default defineConfig({
     plugins: ["import", "typescript"],
     rules: {
       "import/extensions": "error",
-      "no-param-reassign": "error",
-      "default-param-last": "error",
-      "one-var": ["error", "never"],
-      "no-useless-template-literals": "error",
-      "no-else-return": "error",
-      "@typescript-eslint/prefer-as-const": "error",
-      "@typescript-eslint/prefer-enum-initializers": "error",
       "@typescript-eslint/no-inferrable-types": "error",
     },
   },
