1d17af425b85f194ada9c8742893c02bdf2f4468
progfay <19568747+progfay@users.noreply.github.com>
2026-03-17 12:49:22 +0900

    refactor: remove deps for `node:`

--- test/page/index.test.ts
+++ test/page/index.test.ts
@@ -1,7 +1,94 @@
-import * as fs from "node:fs";
-import path from "node:path";
 import { describe, expect, it } from "vite-plus/test";
 import { parse } from "../../src/index.ts";
+
+const HELP_SYNTAX_PAGE_CONTENT = `
+Syntax
+[https://gyazo.com/0f82099330f378fe4917a1b4a5fe8815]
+
+
+[[Internal Links]] (linking to another page on scrapbox)
+	\`[link]\` ⇒ [Link]
+
+[[External  Links]] (linking to another web page)
+ \`http://google.com\` ⇒ http://google.com
+	\`[http://google.com Google]\` ⇒ [http://google.com Google]
+or
+ \`[Google http://google.com]\` ⇒ [Google http://google.com]
+
+[[Images]]
+	Direct image link ↓\`[https://gyazo.com/da78df293f9e83a74b5402411e2f2e01.png]\`
+ [https://i.gyazo.com/da78df293f9e83a74b5402411e2f2e01.png]
+
+[[Clickable Thumbnail Links]]
+	↓ \`[http://cutedog.com https://i.gyazo.com/da78df293f9e83a74b5402411e2f2e01.png]\` 
+ [http://cutedog.com https://i.gyazo.com/da78df293f9e83a74b5402411e2f2e01.png]
+ Adding the link at the end also works, as before:
+  \`[https://i.gyazo.com/da78df293f9e83a74b5402411e2f2e01.png http://cutedog.com]\`
+
+[[Linking to other scrapbox projects]]
+ \`[/projectname/pagename]\` ⇛ [/icons/check]
+ \`[/projectname]\` ⇛ [/icons]
+
+[[Icons]]
+ \`[ben.icon]\` ⇛  [ben.icon]
+ \`[/icons/todo.icon]\` ⇛ [/icons/todo.icon]
+
+[[Bold text]]
+	\`[[Bold]]\` or \`[* Bold]\`⇒ [[Bold]]
+
+[[Italic text]]
+	\`[/ italic]\`⇛ [/ italic]
+
+[[ Strikethrough text]]
+ \`[- strikethrough]\`⇛ [- strikethrough]
+[https://gyazo.com/00ab07461d502db91c8ae170276d1396]
+
+[[Bullet points]]
+	Press space or tab on a new line to indent and create a bullet point
+ 	Press backspace to remove the indent  / bullet point
+
+[[Internal links serve triple duty]]
+	\`[Links]\` or \`#links\` are two ways to make links. They do three things
+		An internal link to a page
+		A [Bi-directional link] back to the source
+		A [2-hop link] so you can find more related pages
+
+[[Block quote]]
+> use the right caret \`>\` at the beginning of a line to get a block quote 
+
+[* Mouse based styling]
+[https://gyazo.com/a515ab169b1e371641f7e04bfa92adbc]
+[[[Code notation]]]
+ Use backquotes or backticks, \`,  to highlight code  
+ e.g. \`function() {  return true }\`
+
+[[[Code blocks]]]
+ Typing \`code:filename.extension\`or\`code:filename\`can be used to create a new code snippet and and display it as a block
+  Language names may be abbreviated
+ code:hello.js
+ 	function () {
+    alert(document.location.href)
+    console.log("hello")
+    // You can also write comments!
+  }
+
+[[[Tables]]]
+	Type table: tablename to create a table
+ Use tab to move to the next column, use enter to move to the next row.
+	An example:
+table:hello
+	1	2	3
+	1 	2 	3
+ ------	------	------
+ a	b	c
+
+
+[* [Mathematical notation]]
+	Using [TeX https://en.wikipedia.org/wiki/TeX] inside of brackets with a dollar sign \`[$ TeX here ]\`, you can format math or science formulas, like so: [$ E = mc^2]
+
+[* [Userscript]]
+	You can even add javascript to customize Scrapbox to your liking.
+`.trimStart();
 
 describe("page", () => {
   it("Empty page", () => {
@@ -15,7 +102,6 @@ describe("page", () => {
   });
 
   it("https://scrapbox.io/help/Syntax", () => {
-    const input = fs.readFileSync(path.resolve(import.meta.dirname, "input.txt")).toString();
-    expect(parse(input, { hasTitle: true })).toMatchSnapshot();
+    expect(parse(HELP_SYNTAX_PAGE_CONTENT, { hasTitle: true })).toMatchSnapshot();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { resolve } from "node:path";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
   pack: {
-    entry: resolve(import.meta.dirname, "src/index.ts"),
+    entry: "src/index.ts",
     dts: true,
     minify: true,
     sourcemap: true,
